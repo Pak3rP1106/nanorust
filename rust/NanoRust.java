48dabcfe05cf2595d03e8e354e57d3790f16b1cf
Pak3rP1106 <54620711+Pak3rP1106@users.noreply.github.com>
2026-09-16 19:53:51 +0200

    Add files via upload

---
+++ rust/NanoRust.java
@@ -0,0 +1,154 @@
+/*     */ package nano.spook1998.rust;
+/*     */ 
+/*     */ import nano.spook1998.rust.data.Manager;
+/*     */ import org.bukkit.plugin.java.JavaPlugin;
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ public class NanoRust
+/*     */   extends JavaPlugin
+/*     */ {
+/*     */   private static NanoRust ALLATORIxDEMO;
+/*     */   
+/*     */   public void onDisable()
+/*     */   {
+/*  41 */     Manager.getInstance().save();
+/*     */   }
+/*     */   
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */   public static NanoRust getInstance()
+/*     */   {
+/*  97 */     return ALLATORIxDEMO;
+/*     */   }
+/*     */   
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */ 
+/*     */   public void onEnable()
+/*     */   {
+/* 146 */     ALLATORIxDEMO = a;Manager.getInstance().load();
+/*     */   }
+/*     */ }
+
+
+/* Location:              C:\Users\User\Desktop\Otek\NanoRust.jar!\nano\spook1998\rust\NanoRust.class
+ * Java compiler version: 8 (52.0)
+ * JD-Core Version:       0.7.1
+ */
